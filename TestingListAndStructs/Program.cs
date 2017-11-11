1789af7b4d08bcb9e2decda7c9864cd0b04b1da2
Darryn <Darryn@DARRYNPC>
2017-11-11 21:42:05 +0000

    Add project files.

---
+++ TestingListAndStructs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingListAndStructs
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // Initialise:   
+            test coords1 = new test(0,0);
+            test coords2 = new test(10, 10);
+
+            List<test> list = new List<test>();
+            list.Add(coords1);
+            list.Add(coords2);
+
+            // Display results:
+
+            foreach (test obj in list)
+            {
+                obj.printout();
+            }
+
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+}
+
---
+++ TestingListAndStructs/test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingListAndStructs
+{
+    class test
+    {
+
+        private int _x, _y;
+
+        public test(int p1, int p2)
+        {
+            _x = p1;
+            _y = p2;
+        }
+
+        public void printout()
+        {
+            Console.WriteLine(_x + " " + _y);
+        }
+    }
+}
